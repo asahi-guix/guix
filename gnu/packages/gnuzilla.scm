c9bb7de95b531ab09244d224210c655f2b37d607
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:13:02 +0200

    gnu: icedove: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/gnuzilla.scm (icedove)[inputs]: Add bash-minimal.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I8662841b88d46baebc7f700b7331b0c5cc786f64

--- gnu/packages/gnuzilla.scm
+++ gnu/packages/gnuzilla.scm
@@ -1808,6 +1808,7 @@ ca495991b7852b855"))
                   `("LD_LIBRARY_PATH" prefix (,pulseaudio-lib ,eudev-lib ,libnotify-lib)))))))))
     (inputs
      (list alsa-lib
+           bash-minimal
            bzip2
            cairo
            cups
