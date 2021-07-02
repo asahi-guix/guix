46f36c8b071675dd238bc04c89df51a32442c3f1
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:06:17 +0200

    gnu: gnome: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/gnome.scm
    (totem)[inputs]: Add 'bash-minimal'.
    (d-feet)[inputs]: Likewise.
    (gnome-session)[inputs]: Likewise.
    (caribou)[inputs]: Likewise.
    (authenticator)[inputs]: Likewise.
    (soundconverter)[inputs]: Likewise.
    (terminator)[inputs]: Likewise.
    (gitg)[inputs]: Likewise.
    (libratbag)[inputs]: Likewise.
    (piper)[inputs]: Likewise.
    (parlatype)[inputs]: Likewise.
    (orcfeeder)[inputs]: Likewise.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I720f33461756950844bfeab04307c8421287f386

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -6753,7 +6753,8 @@ discovery protocols.")
     (propagated-inputs
      (list dconf))
     (inputs
-     (list gtk+
+     (list bash-minimal
+           gtk+
            gdk-pixbuf
            at-spi2-core
            cairo
@@ -7355,7 +7356,8 @@ principles are simplicity and standards compliance.")
            python-pep8
            xorg-server-for-tests))
     (inputs
-     (list gobject-introspection
+     (list bash-minimal
+           gobject-introspection
            gtk+
            python-wrapper
            python-pygobject))
@@ -7756,7 +7758,8 @@ such as gzip tarballs.")
            pkg-config
            xmlto))
     (inputs
-     (list elogind
+     (list bash-minimal
+           elogind
            gnome-desktop
            gsettings-desktop-schemas
            gtk+
@@ -8393,7 +8396,8 @@ Evolution (hence the name), but is now used by other packages as well.")
      ;; caribou-1.0.pc refers to all these.
      (list libgee libxklavier libxtst gtk+))
     (inputs
-     `(("clutter" ,clutter)
+     `(("bash" ,bash-minimal) ; for wrap-program
+       ("clutter" ,clutter)
        ("dconf" ,dconf)
        ("gtk+-2" ,gtk+-2)
        ("python-pygobject" ,python-pygobject)))
@@ -10480,7 +10484,8 @@ specified duration and save it as a GIF encoded animated image file.")
            `(,gtk+ "bin")
            pkg-config))
     (inputs
-     (list gsettings-desktop-schemas
+     (list bash-minimal
+           gsettings-desktop-schemas
            gtk+
            libhandy-0.0
            libsecret
@@ -11859,7 +11864,12 @@ mp3, Ogg Vorbis and FLAC")
        ("pkg-config" ,pkg-config)
        ("glib:bin" ,glib "bin")))
     (inputs
-     (list gtk+ python python-pygobject gstreamer gst-plugins-base))
+     (list bash-minimal
+           gtk+
+           python
+           python-pygobject
+           gstreamer
+           gst-plugins-base))
     (home-page "https://soundconverter.org/")
     (synopsis "Convert between audio formats with a graphical interface")
     (description
@@ -12144,7 +12154,8 @@ advanced image management tool")
        ("python-pytest-runner" ,python-pytest-runner)
        ("python-pytest" ,python-pytest)))
     (inputs
-     `(("cairo" ,cairo)
+     `(("bash" ,bash-minimal) ; for wrap-program
+       ("cairo" ,cairo)
        ("dbus-glib" ,dbus-glib)
        ("gsettings-desktop-schemas" ,gsettings-desktop-schemas)
        ("gtk+" ,gtk+)
@@ -12328,7 +12339,8 @@ higher level porcelain stuff.")
                 (wrap-program prog
                   `("GI_TYPELIB_PATH" = (,(getenv "GI_TYPELIB_PATH"))))))))))
     (inputs
-     (list glib
+     (list bash-minimal
+           glib
            gpgme
            gsettings-desktop-schemas
            gspell
@@ -13019,7 +13031,8 @@ It uses pandoc as back-end for parsing Markdown.")
          (list valgrind)
          '())))
     (inputs
-     `(("glib" ,glib)
+     `(("bash" ,bash-minimal) ; for wrap-program
+       ("glib" ,glib)
        ("json-glib" ,json-glib)
        ("libevdev" ,libevdev)
        ("libsystemd" ,elogind)
@@ -13090,7 +13103,8 @@ your operating-system definition:
            gobject-introspection
            pkg-config))
     (inputs
-     (list adwaita-icon-theme
+     (list bash-minimal
+           adwaita-icon-theme
            gtk+
            guile-3.0                    ;for wrap-script
            libratbag
@@ -13191,7 +13205,8 @@ provided there is a DBus service present:
            pkg-config
            yelp-tools))
     (inputs
-     (list gst-plugins-base
+     (list bash-minimal
+           gst-plugins-base
            gst-plugins-good
            gstreamer
            gtk+
@@ -13711,7 +13726,8 @@ world.")
        ("pkg-config" ,pkg-config)
        ("xmllint" ,libxml2)))
     (inputs
-     `(("enchant" ,enchant)
+     `(("bash" ,bash-minimal) ; for wrap-program
+       ("enchant" ,enchant)
        ("glib" ,glib)
        ("goocanvas" ,goocanvas)
        ("gtk" ,gtk+)
