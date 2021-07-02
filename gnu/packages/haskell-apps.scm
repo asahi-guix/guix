10f4b42f46bd0e79d8082c8bfe2901f1f57c9432
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:13:10 +0200

    gnu: raincat: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/haskell-apps.scm
    (raincat)[inputs]: Add 'bash-minimal'.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I43678905da46d4ab6638d2c7744a547d5ee00089

--- gnu/packages/haskell-apps.scm
+++ gnu/packages/haskell-apps.scm
@@ -42,6 +42,7 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system haskell)
   #:use-module (gnu packages)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages gl)
@@ -764,7 +765,8 @@ Haskell.")
                                     "/lib"))))
                #t))))))
     (inputs
-     (list ghc-extensible-exceptions
+     (list bash-minimal
+           ghc-extensible-exceptions
            ghc-random
            ghc-glut
            freeglut
