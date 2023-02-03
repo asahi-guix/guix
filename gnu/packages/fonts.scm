5646392fbad4f127b2b1f24ec39b93e1163b1f82
Simon South <simon@simonsouth.net>
2023-02-03 07:43:01 -0500

    gnu: font-lato: Install license file.
    * gnu/packages/fonts.scm (font-lato)[source]: Use url-fetch.

--- gnu/packages/fonts.scm
+++ gnu/packages/fonts.scm
@@ -395,7 +395,7 @@ of the font.")
     (name "font-lato")
     (version "2.015")                   ; also update description
     (source (origin
-              (method url-fetch/zipbomb)
+              (method url-fetch)
               (uri (string-append "https://www.latofonts.com/download/Lato2OFL.zip"))
               (sha256
                (base32
