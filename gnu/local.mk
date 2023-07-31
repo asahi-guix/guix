25b5ce6833839d2ef1936598d65e6f767b0376cc
Danny Milosavljevic <dannym@scratchpost.org>
2023-07-31 20:09:17 +0200

    gnu: openjdk13: Make reproducible.
    * gnu/packages/patches/openjdk-13-classlist-reproducibility.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add patch.
    * gnu/packages/java.scm (openjdk13)[source]: Add patch.
    [arguments]<#:phases>[remove-timestamping]: Modify phase.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I1b2943c989e2d6acfd8131ea4ede978c61948811

--- gnu/local.mk
+++ gnu/local.mk
@@ -1838,6 +1838,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/openjdk-10-pointer-comparison.patch      \
   %D%/packages/patches/openjdk-10-setsignalhandler.patch        \
   %D%/packages/patches/openjdk-11-classlist-reproducibility.patch	\
+  %D%/packages/patches/openjdk-13-classlist-reproducibility.patch	\
   %D%/packages/patches/openjdk-15-xcursor-no-dynamic.patch	\
   %D%/packages/patches/openjdk-21-fix-rpath.patch		\
   %D%/packages/patches/openmpi-mtl-priorities.patch		\
