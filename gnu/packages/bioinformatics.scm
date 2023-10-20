2e283b28dd584ba51a3a7f806aa8b0a2668f5287
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-20 01:09:46 -0400

    gnu: arriba: Add bash-minimal to inputs.
    * gnu/packages/bioinformatics.scm (arriba)
    [inputs]: Add bash-minimal.
    
    Change-Id: Ib30f9e0872dc24ca24e498eba51560c750e75d22

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -19743,7 +19743,8 @@ includes a command line tool and an analysis pipeline.")
                 (wrap-program (string-append bin "/draw_fusions.R")
                   `("R_LIBS_SITE" ":" prefix (,(getenv "R_LIBS_SITE"))))))))))
     (inputs
-     (list htslib
+     (list bash-minimal
+           htslib
            r-minimal
            r-circlize
            r-genomicalignments
