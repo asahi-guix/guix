f16a4164df09866ee60a095af1846f618621ebd7
Josselin Poiret <dev@jpoiret.xyz>
2024-02-02 12:15:55 +0100

    gnu: elogind: Add libxcrypt dependency.
    * gnu/packages/freedesktop.scm (elogind): Add libxcrypt dependency.
    
    Change-Id: I7259b20a96c50a7f88cb0eb7ff30d32f233d0bfe

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -81,6 +81,7 @@
   #:use-module (gnu packages check)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages compression)
+  #:use-module (gnu packages crypto)
   #:use-module (gnu packages cryptsetup)
   #:use-module (gnu packages cups)
   #:use-module (gnu packages curl)
@@ -893,6 +894,7 @@ the freedesktop.org XDG Base Directory specification.")
           '())
       (list linux-pam
             libcap
+            libxcrypt
             `(,util-linux "lib")        ;for 'libmount'
             shadow                      ;for 'nologin'
             shepherd                    ;for 'halt' and 'reboot', invoked
