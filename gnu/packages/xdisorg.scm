4f610d376a64da872aed5433af2f711d8adb4c78
Josselin Poiret <dev@jpoiret.xyz>
2024-03-04 23:24:58 +0100

    gnu: xlockmore: Add libxcrypt dependency.
    * gnu/packages/xdisorg.scm (xlockmore): Add libxcrypt dependency.
    
    Change-Id: Idfaed843b40db41b3c7c64cbb32765cefc28238e

--- gnu/packages/xdisorg.scm
+++ gnu/packages/xdisorg.scm
@@ -105,6 +105,7 @@
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages cpp)
+  #:use-module (gnu packages crypto)
   #:use-module (gnu packages datastructures)
   #:use-module (gnu packages docbook)
   #:use-module (gnu packages documentation)
@@ -1188,7 +1189,7 @@ shows it again when the mouse cursor moves or a mouse button is pressed.")
                              "/lib/X11/app-defaults"))
       #:tests? #f))                     ;no such thing as a test suite
     (inputs
-     (list libx11 libxext libxt linux-pam))
+     (list libx11 libxcrypt libxext libxt linux-pam))
     (home-page "https://sillycycle.com/xlockmore.html")
     (synopsis "Screen locker for the X Window System")
     (description
